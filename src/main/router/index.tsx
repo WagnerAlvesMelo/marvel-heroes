702ec1f0cd784e3134cf412760627d5afa63314a
Wagner Melo <walves13@outlook.com>
2022-07-05 00:03:01 -0300

    chore: setup deploy

--- src/main/router/index.tsx
+++ src/main/router/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from 'presentation/pages/Home';
 import CharacterDetails from 'presentation/pages/Character/CharacterDetails';
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="character/:id" element={<CharacterDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
