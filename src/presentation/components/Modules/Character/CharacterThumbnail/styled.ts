8311e15320eda872bbce06decb910f36fb06946b
Wagner Melo <walves13@outlook.com>
2022-07-06 00:32:08 -0300

    fix: thumbnail on moz browsers

--- src/presentation/components/Modules/Character/CharacterThumbnail/styled.ts
+++ src/presentation/components/Modules/Character/CharacterThumbnail/styled.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Wrapper = styled(Link)`
-  width: 50%;
+  display: block;
+  width: 100%;
   position: relative;
   cursor: pointer;
 
